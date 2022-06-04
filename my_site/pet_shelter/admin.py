4257f759e5e6c3f691a0fe268e62cf9e20f13534
Dimazey <mof@list.ru>
2022-06-04 13:55:29 +0300

    first commit

--- my_site/pet_shelter/admin.py
+++ my_site/pet_shelter/admin.py
@@ -2,17 +2,21 @@ from django.contrib import admin
 from pet_shelter.models import Pet, Owner, Breed, Color
 # Register your models here.
 
+
 @admin.register(Pet)
 class PetAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     pass
 
+
 @admin.register(Breed)
 class BreedAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Color)
 class ColorAdmin(admin.ModelAdmin):
